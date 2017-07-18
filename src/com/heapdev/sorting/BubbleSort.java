3d39a00ebb679b0af785a3374c449fa426e07c6b
Abhinav <singh09.abhinav@gmail.com>
2017-07-19 00:27:08 +0530

    commited with bubble sort implmentation

---
+++ src/com/heapdev/sorting/BubbleSort.java
@@ -0,0 +1,44 @@
+package com.heapdev.sorting;
+
+/**
+ * 
+ * Bubbe sort is very impractical in use. The Time complexity is O(N^2)
+ * 
+ * @author singh09.abhinav@gmail.com
+ *
+ */
+public class BubbleSort {
+
+	public static void main(String[] args) {
+
+		int[] nums = { 11, -7, 12, 4, 8, 1 };
+
+		showArray(nums);
+		bubbleSort(nums);
+		System.out.println();
+		showArray(nums);
+
+	}
+
+	public static void bubbleSort(int[] arr) {
+		for (int i = 0; i < arr.length - 1; i++) {
+			for (int j = 0; j < arr.length - 1 - i; j++) {
+				if (arr[j] > arr[j + 1]) {
+					swap(arr, j, j + 1);
+				}
+			}
+		}
+	}
+
+	private static void swap(int[] arr, int j, int i) {
+		int temp = arr[j];
+		arr[j] = arr[i];
+		arr[i] = temp;
+	}
+
+	public static void showArray(int[] arr) {
+		for (int i : arr) {
+			System.out.print(i + " ");
+		}
+	}
+}
